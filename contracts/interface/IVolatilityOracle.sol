d495d250fa47af4e6e503f34fed8ebfd4a9d5f45
Richard Chen <yuechao.chen@bit-space.net>
2021-07-15 10:03:12 +0800

    correct volatility spell error

--- contracts/interface/IVolatilityOracle.sol
+++ contracts/interface/IVolatilityOracle.sol
@@ -4,8 +4,8 @@ pragma solidity >=0.8.0 <0.9.0;
 
 interface IVolatilityOracle {
 
-    function getVolitility() external view returns (uint256);
+    function getVolatility() external view returns (uint256);
 
-    function updateVolitility(uint256 timestamp_, uint256 volitility_, uint8 v_, bytes32 r_, bytes32 s_) external;
+    function updateVolatility(uint256 timestamp_, uint256 volatility_, uint8 v_, bytes32 r_, bytes32 s_) external;
 
 }
--- contracts/utils/VolatilityOracleOffChain.sol
+++ contracts/utils/VolatilityOracleOffChain.sol
@@ -11,7 +11,7 @@ contract VolatilityOracleOffChain is IVolatilityOracle {
     uint256 public delayAllowance;
 
     uint256 public timestamp;
-    uint256 public volitility;
+    uint256 public volatility;
 
     constructor (string memory symbol_, address signatory_, uint256 delayAllowance_) {
         symbol = symbol_;
@@ -24,23 +24,23 @@ contract VolatilityOracleOffChain is IVolatilityOracle {
         delayAllowance = delayAllowance_;
     }
 
-    function getVolitility() external override view returns (uint256) {
-        require(block.timestamp - timestamp < delayAllowance, 'volitility expired');
-        return volitility;
+    function getVolatility() external override view returns (uint256) {
+        require(block.timestamp - timestamp < delayAllowance, 'volatility expired');
+        return volatility;
     }
 
-    // update oracle volitility using off chain signed volitility
-    // the signature must be verified in order for the volitility to be updated
-    function updateVolitility(uint256 timestamp_, uint256 volitility_, uint8 v_, bytes32 r_, bytes32 s_) external override {
+    // update oracle volatility using off chain signed volatility
+    // the signature must be verified in order for the volatility to be updated
+    function updateVolatility(uint256 timestamp_, uint256 volatility_, uint8 v_, bytes32 r_, bytes32 s_) external override {
         uint256 lastTimestamp = timestamp;
         if (timestamp_ > lastTimestamp) {
             if (v_ == 27 || v_ == 28) {
-                bytes32 message = keccak256(abi.encodePacked(symbol, timestamp_, volitility_));
+                bytes32 message = keccak256(abi.encodePacked(symbol, timestamp_, volatility_));
                 bytes32 hash = keccak256(abi.encodePacked('\x19Ethereum Signed Message:\n32', message));
                 address signer = ecrecover(hash, v_, r_, s_);
                 if (signer == signatory) {
                     timestamp = timestamp_;
-                    volitility = volitility_;
+                    volatility = volatility_;
                 }
             }
         }
